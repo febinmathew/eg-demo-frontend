c5c00a62cc98531cb22a913fbd7ede107febf6cc
Febin Mathew <fmfebinmathew@gmail.com>
2024-09-04 14:50:04 +0530

    Add Register Component

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,5 +1,61 @@
+import { useState } from "react";
+import axios from "../../api/axios";
+import { useNavigate } from "react-router-dom";
+
 function Register() {
-  return <h1>Register</h1>;
+  const [name, setName] = useState<string>("Febin Mathew");
+  const [email, setEmail] = useState<string>("fmfebinmathew4@gmail.com");
+  const [password, setPassword] = useState<string>("f$123sdfsdfsdf");
+  const navigate = useNavigate();
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post<{ email: string; name: string }>(
+        "/auth/register",
+        {
+          name,
+          email,
+          password,
+        }
+      );
+
+      navigate("/login");
+    } catch (error) {
+      console.error("Login failed", error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleRegister}>
+        <label>
+          Name:
+          <input
+            type="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -6,6 +6,10 @@ export const getToken = (): string | null => {
   return localStorage.getItem("token");
 };
 
+export const removeToken = (): void => {
+  localStorage.removeItem("token");
+};
+
 export const isAuthenticated = (): boolean => {
   const token = getToken();
   if (!token) return false;
